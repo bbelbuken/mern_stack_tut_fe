e66a0591c676bb9465829482e9f2b0396debe89d
Batuhan Belbüken <bbelbuken@gmail.com>
2025-02-06 23:49:37 +0300

    authController refresh endpoint created

--- be_05_authentication_authorization/controllers/authController.js
+++ be_05_authentication_authorization/controllers/authController.js
@@ -31,13 +31,13 @@ const login = asyncHandler(async (req, res) => {
             },
         },
         process.env.ACCESS_TOKEN_SECRET,
-        { expiresIn: '15m' }
+        { expiresIn: '10s' }
     );
 
     const refreshToken = jwt.sign(
         { username: foundUser.username },
         process.env.REFRESH_TOKEN_SECRET,
-        { expiresIn: '7d' }
+        { expiresIn: '1d' }
     );
 
     // Create secure cookie with refresh token
@@ -51,6 +51,45 @@ const login = asyncHandler(async (req, res) => {
     // Send accessToken containing username and roles
     res.json({ accessToken });
 });
+
+// @desc Refresh
+// @route GET /auth/refresh
+// @access Public - because access token has expired
+const refresh = (req, res) => {
+    const cookies = req.cookies;
+
+    if (!cookies?.jwt) return res.status(401).json({ message: 'Unauthorized' });
+
+    const refreshToken = cookies.jwt;
+
+    jwt.verify(
+        refreshToken,
+        process.env.REFRESH_TOKEN_SECRET,
+        asyncHandler(async (err, decoded) => {
+            if (err) return res.status(403).json({ message: 'Forbidden' });
+
+            const foundUser = await User.findOne({
+                username: decoded.username,
+            }).exec();
+
+            if (!foundUser)
+                return res.status(401).json({ message: 'Unauthorized' });
+
+            const accessToken = jwt.sign(
+                {
+                    UserInfo: {
+                        username: foundUser.username,
+                        roles: foundUser.roles,
+                    },
+                },
+                process.env.ACCESS_TOKEN_SECRET,
+                { expiresIn: '10s' }
+            );
+
+            res.json({ accessToken });
+        })
+    );
+};
 
 module.exports = {
     login,
