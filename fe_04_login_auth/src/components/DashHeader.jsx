bbd185c2dc2e1c1bfd72d3f619bb6819d4b2e40b
Batuhan Belbüken <bbelbuken@gmail.com>
2025-02-08 19:14:22 +0300

    implementing logout to dashHeader

--- fe_04_login_auth/src/components/DashHeader.jsx
+++ fe_04_login_auth/src/components/DashHeader.jsx
@@ -1,5 +1,13 @@
-import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate, Link, useLocation } from 'react-router-dom';
 
+import { useSendLogoutMutation } from '../features/auth/authApiSlice';
+
+const DASH_REGEX = /^\/dash(\/)?$/;
+const NOTES_REGEX = /^\/dash\/notes(\/)?$/;
+const USERS_REGEX = /^\/dash\/users(\/)?$/;
 const DashHeader = () => {
     return (
         <header className='dash-header'>
