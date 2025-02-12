7ed611dfb72fdad64b7b9b0838f84bcd4a3cf478
Batuhan Belbüken <bbelbuken@gmail.com>
2025-02-13 01:20:47 +0300

    ready for deployment

--- be_07_deployment/config/allowedOrigins.js
+++
@@ -1,9 +0,0 @@
-const allowedOrigins = [
-    'http://localhost:3000',
-    'http://localhost:3500',
-    'http://www.dandrepairshop.com',
-    'http://dandrepairshop.com',
-    'https://www.google.com',
-];
-
-module.exports = allowedOrigins;
